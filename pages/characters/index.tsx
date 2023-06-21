f81ddf2de6111447737e198643037d180c620d01
polyakog <gpolyakov77@gmail.com>
2023-06-21 19:59:17 +0400

    feat: add characters component

---
+++ pages/characters/index.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image"
+import { API } from "../../assets/api/api"
+import { CharacterType, ResponseType } from "../../assets/api/rick-and-morty-api"
+import { Header } from "../../components/Header/Header"
+import { PageWrapper } from "../../components/PageWrapper/PageWrapper"
+
+export const getStaticProps =async () => {
+    const characters = await API.rickAndMorty.getCharacters()
+    return {
+        props: {
+            characters
+        }
+    }      
+
+}
+
+type PropsType = {
+    characters: ResponseType<CharacterType>
+}
+
+const Characters = (props: PropsType) => {
+    const {characters}= props
+
+    const charactersList = characters.results.map(c => (
+        <div key={c.id}>
+            <Image height= '50' width = '50' src={c.image} alt={c.name} />
+            <div>{c.name}</div> 
+            
+            
+            </div>
+    ))
+
+return(
+    <PageWrapper>
+        <Header />
+        {charactersList} 
+    </PageWrapper>
+)
+
+}
+
+export default Characters
