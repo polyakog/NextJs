04ba0700fa74298086d9e97d8bb6381d9f285cd3
polyakog <gpolyakov77@gmail.com>
2023-06-23 11:06:53 +0400

    feat: create useLoader and add to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import {ReactElement, ReactNode} from 'react';
 import {NextPage} from 'next';
 import { QueryClient, QueryClientProvider, Hydrate } from '@tanstack/react-query';
 import React, { useState } from 'react';
+import { useLoader } from '../assets/hooks/useLoader';
+import '../styles/nprogress.css'
 
 
 export type NextPageWithLayout<P = {}> = NextPage<P> & {
@@ -15,6 +17,8 @@ type AppPropsWithLayout = AppProps & {
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const [queryClient] = useState(() => new QueryClient)
+  
+  useLoader()
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return getLayout(
